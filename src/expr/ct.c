a7dfa49d34c76a420778a6fce2b5e7d85f0c8b1a
Zhongqiu Duan <dzq.aishenghu0@gmail.com>
2025-03-19 14:29:27 +0000

    expr: ct: print key name of id field
    Fixes: 005369151ed5 ("include: updated nf_tables.h")
    Signed-off-by: Zhongqiu Duan <dzq.aishenghu0@gmail.com>
    Signed-off-by: Florian Westphal <fw@strlen.de>

--- src/expr/ct.c
+++ src/expr/ct.c
@@ -171,7 +171,7 @@ static const char *ctkey2str_array[NFT_CT_MAX + 1] = {
 
 static const char *ctkey2str(uint32_t ctkey)
 {
-	if (ctkey >= NFT_CT_MAX)
+	if (ctkey > NFT_CT_MAX)
 		return "unknown";
 
 	return ctkey2str_array[ctkey];
