greearb/libnftnl-ct

commit 26c945057d742fc4b0f4dfdc07849074cb9264c1

src: split internal.h is smaller files
The internal.h file started being a small file with private definitions.
Its size has been increasing over time more and more, so let's split
this in small header files that map to the corresponding class where the
functions belong to.

Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

include/utils.h
@@ -1,0 +1,83 @@
+#ifndef LIBNFTNL_UTILS_H
+#define LIBNFTNL_UTILS_H 1
+
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include <libnftnl/common.h>
+
+#include "config.h"
+#ifdef HAVE_VISIBILITY_HIDDEN
+#	define __visible	__attribute__((visibility("default")))
+#	define EXPORT_SYMBOL(x)	typeof(x) (x) __visible
+#else
+#	define EXPORT_SYMBOL
+#endif
+
+#define __init		__attribute__((constructor))
+#define __noreturn	__attribute__((__noreturn__))
+
+#define xfree(ptr)	free((void *)ptr);
+
+#define div_round_up(n, d)	(((n) + (d) - 1) / (d))
+
+void __noreturn __abi_breakage(const char *file, int line, const char *reason);
+
+#define abi_breakage()	\
+	__abi_breakage(__FILE__, __LINE__, strerror(errno));
+
+void __nft_assert_fail(uint16_t attr, const char *filename, int line);
+
+#define nft_assert(val, attr, expr)			\
+  ((!val || expr)					\
+   ? (void)0						\
+   : __nft_assert_fail(attr, __FILE__, __LINE__))
+
+#define nft_assert_validate(data, _validate_array, _attr, _data_len)		\
+({										\
+	if (!data)								\
+		__nft_assert_fail(attr, __FILE__, __LINE__);			\
+	if (_validate_array[_attr])						\
+		nft_assert(data, attr, _validate_array[_attr] == _data_len);	\
+})
+
+#define SNPRINTF_BUFFER_SIZE(ret, size, len, offset)	\
+	if (ret < 0)					\
+		return ret;				\
+	offset += ret;					\
+	if (ret > len)					\
+		ret = len;				\
+	size += ret;					\
+	len -= ret;
+
+const char *nft_family2str(uint32_t family);
+int nft_str2family(const char *family);
+
+enum nft_type {
+	NFT_TYPE_U8,
+	NFT_TYPE_U16,
+	NFT_TYPE_U32,
+	NFT_TYPE_U64,
+	NFT_TYPE_S8,
+	NFT_TYPE_S16,
+	NFT_TYPE_S32,
+	NFT_TYPE_S64,
+};
+
+int nft_strtoi(const char *string, int base, void *number, enum nft_type type);
+int nft_get_value(enum nft_type type, void *val, void *out);
+
+const char *nft_verdict2str(uint32_t verdict);
+int nft_str2verdict(const char *verdict, int *verdict_num);
+
+const char *nft_cmd2tag(enum nft_cmd_type cmd);
+uint32_t nft_str2cmd(const char *cmd);
+
+enum nft_cmd_type nft_flag2cmd(uint32_t flags);
+
+int nft_fprintf(FILE *fp, void *obj, uint32_t cmd, uint32_t type,
+		uint32_t flags, int (*snprintf_cb)(char *buf, size_t bufsiz,
+		void *obj, uint32_t cmd, uint32_t type, uint32_t flags));
+
+#endif

include/Makefile.am
@@ -1,4 +1,15 @@
 SUBDIRS = libnftnl linux
 
-noinst_HEADERS = linux_list.h	\
-		 buffer.h
+noinst_HEADERS = internal.h	\
+		 linux_list.h	\
+		 buffer.h	\
+		 data_reg.h	\
+		 expr_ops.h	\
+		 linux_list.h	\
+		 set.h		\
+		 xml.h		\
+		 common.h	\
+		 expr.h		\
+		 json.h		\
+		 set_elem.h	\
+		 utils.h

include/common.h
@@ -1,0 +1,32 @@
+#ifndef _LIBNFTNL_COMMON_INTERNAL_H
+#define _LIBNFTNL_COMMON_INTERNAL_H
+
+#define BASE_DEC 10
+#define BASE_HEX 16
+
+#define NFT_SNPRINTF_BUFSIZ 4096
+
+struct nft_parse_err {
+	int line;
+	int column;
+	int error;
+	const char *node_name;
+};
+
+enum nft_parse_input {
+	NFT_PARSE_BUFFER,
+	NFT_PARSE_FILE,
+};
+
+#include <stdio.h>
+
+int nft_cmd_header_snprintf(char *buf, size_t bufsize, uint32_t cmd,
+			   uint32_t format, uint32_t flags);
+int nft_cmd_header_fprintf(FILE *fp, uint32_t cmd, uint32_t format,
+			  uint32_t flags);
+int nft_cmd_footer_snprintf(char *buf, size_t bufsize, uint32_t cmd,
+			   uint32_t format, uint32_t flags);
+int nft_cmd_footer_fprintf(FILE *fp, uint32_t cmd, uint32_t format,
+			  uint32_t flags);
+
+#endif

include/data_reg.h
@@ -1,6 +1,9 @@
 #ifndef _DATA_H_
 #define _DATA_H_
 
+#include <stdint.h>
+#include <unistd.h>
+
 enum {
 	DATA_NONE,
 	DATA_VALUE,
@@ -19,20 +22,10 @@ union nft_data_reg {
 	};
 };
 
-#ifndef JSON_PARSING
-#define json_t void
-#endif
-
-#ifndef XML_PARSING
-#define mxml_node_t void
-#endif
-
 int nft_data_reg_snprintf(char *buf, size_t size, union nft_data_reg *reg,
                         uint32_t output_format, uint32_t flags, int reg_type);
-int nft_data_reg_xml_parse(union nft_data_reg *reg, mxml_node_t *tree,
-			   struct nft_parse_err *err);
+struct nlattr;
+
 int nft_parse_data(union nft_data_reg *data, struct nlattr *attr, int *type);
-int nft_data_reg_json_parse(union nft_data_reg *reg, json_t *data,
-			    struct nft_parse_err *err);
 
 #endif

include/expr.h
@@ -1,0 +1,13 @@
+#ifndef _LIBNFTNL_EXPR_INTERNAL_H_
+#define _LIBNFTNL_EXPR_INTERNAL_H_
+
+struct expr_ops;
+
+struct nft_rule_expr {
+	struct list_head	head;
+	uint32_t		flags;
+	struct expr_ops		*ops;
+	uint8_t			data[];
+};
+
+#endif

include/expr_ops.h
@@ -1,21 +1,12 @@
 #ifndef _EXPR_OPS_H_
 #define _EXPR_OPS_H_
 
-#include "internal.h"
-#include <stdlib.h>
 #include <stdint.h>
+#include "internal.h"
 
 struct nlattr;
 struct nlmsghdr;
 struct nft_rule_expr;
-
-#ifndef XML_PARSING
-#define mxml_node_t void
-#endif
-
-#ifndef JSON_PARSING
-#define json_t void
-#endif
 
 struct expr_ops {
 	struct list_head head;

include/internal.h
@@ -1,0 +1,18 @@
+#ifndef _LIBNFTNL_INTERNAL_H_
+#define _LIBNFTNL_INTERNAL_H_
+
+/* The headers below are NOT exposed as part of the API. */
+#include "data_reg.h"
+#include "linux_list.h"
+#include "utils.h"
+#include "common.h"
+#include "xml.h"
+#include "json.h"
+#include "linux_list.h"
+#include "set.h"
+#include "set_elem.h"
+#include "expr.h"
+#include "expr_ops.h"
+#include "buffer.h"
+
+#endif /* _LIBNFTNL_INTERNAL_H_ */

include/json.h
@@ -1,0 +1,58 @@
+#ifndef LIBNFTNL_JSON_INTERNAL_H
+#define LIBNFTNL_JSON_INTERNAL_H
+
+#ifdef JSON_PARSING
+#include <jansson.h>
+#include <stdbool.h>
+#include "common.h"
+
+struct nft_table;
+struct nft_chain;
+struct nft_rule;
+struct nft_set;
+struct nft_set_elem;
+struct nft_set_list;
+union nft_data_reg;
+
+int nft_jansson_parse_val(json_t *root, const char *node_name, int type,
+			  void *out, struct nft_parse_err *err);
+const char *nft_jansson_parse_str(json_t *root, const char *node_name,
+				  struct nft_parse_err *err);
+bool nft_jansson_node_exist(json_t *root, const char *node_name);
+json_t *nft_jansson_create_root(const void *json, json_error_t *error,
+				struct nft_parse_err *err, enum nft_parse_input input);
+json_t *nft_jansson_get_node(json_t *root, const char *node_name,
+			     struct nft_parse_err *err);
+void nft_jansson_free_root(json_t *root);
+int nft_jansson_parse_family(json_t *root, void *out, struct nft_parse_err *err);
+int nft_jansson_str2num(json_t *root, const char *node_name, int base, void *out,
+			enum nft_type type, struct nft_parse_err *err);
+int nft_jansson_parse_reg(json_t *root, const char *node_name, int type,
+			  void *out, struct nft_parse_err *err);
+struct nft_rule_expr *nft_jansson_expr_parse(json_t *root,
+					     struct nft_parse_err *err,
+					     struct nft_set_list *set_list);
+int nft_jansson_data_reg_parse(json_t *root, const char *node_name,
+			       union nft_data_reg *data_reg,
+			       struct nft_parse_err *err);
+int nft_jansson_set_elem_parse(struct nft_set_elem *e, json_t *root,
+			       struct nft_parse_err *err);
+int nft_jansson_parse_table(struct nft_table *t, json_t *tree,
+			    struct nft_parse_err *err);
+int nft_jansson_parse_chain(struct nft_chain *c, json_t *tree,
+			    struct nft_parse_err *err);
+int nft_jansson_parse_rule(struct nft_rule *r, json_t *tree,
+			   struct nft_parse_err *err,
+			   struct nft_set_list *set_list);
+int nft_jansson_parse_set(struct nft_set *s, json_t *tree,
+			  struct nft_parse_err *err);
+int nft_jansson_parse_elem(struct nft_set *s, json_t *tree,
+			   struct nft_parse_err *err);
+
+int nft_data_reg_json_parse(union nft_data_reg *reg, json_t *data,
+			    struct nft_parse_err *err);
+#else
+#define json_t void
+#endif
+
+#endif /* LIBNFTNL_JSON_INTERNAL_H */

1g4-mirror/libnftnl

commit 7a7722ee336fac14690851f6c84965582a741601

include: utils.h needs errno.h
Otherwise includers may face 'errno' undeclared errors.

Fixes: 26c945057d742 ("src: split internal.h is smaller files")
Signed-off-by: Phil Sutter <phil@nwl.cc>
Signed-off-by: Florian Westphal <fw@strlen.de>

include/utils.h
@@ -1,6 +1,7 @@
 #ifndef LIBNFTNL_UTILS_H
 #define LIBNFTNL_UTILS_H 1
 
+#include <errno.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>